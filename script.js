fa86145602ab1adb99a4af5ad4c42a5d664b9196
murray <murray.buchanan.2022@uni.strath.ac.uk>
2024-04-01 21:59:48 +0100

    Uncountable changes

--- script.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('scroll', function() {
-    var scrollSection = document.querySelector('.scroll-section');
-    if (window.scrollY > 0) {
-        scrollSection.style.opacity = '0'; // Fade out
-    } else {
-        scrollSection.style.opacity = '1'; // Fade in
-    }
-});
